bd12ba1ec97f6cd971133633febc0b8e36f2eb6e
Thomas Hunziker <github@bakual.ch>
2026-01-12 14:31:20 +0100

    Add Station Field for error detection if the weather module isn't installed (and thus database table is missing)

---
+++ Field/StationField.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * @package     Wetterwarnungen
+ * @author      Thomas Hunziker <admin@bakual.net>
+ * @copyright   © 2026 - Thomas Hunziker
+ * @license     https://www.gnu.org/licenses/gpl.html
+ **/
+
+namespace Bakual\Module\Wetterwarnungen\Site\Field;
+
+use Joomla\CMS\Form\Field\SqlField;
+use Joomla\CMS\Helper\ModuleHelper;
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Language\Text;
+
+defined('_JEXEC') or die();
+
+/**
+ * Dateformat Field class for the SermonSpeaker.
+ * Based on the Bannerlist field from com_banners
+ *
+ * @package        SermonSpeaker
+ * @since          4.0
+ */
+class StationField extends SqlField
+{
+	/**
+	 * The form field type.
+	 *
+	 * @var        string
+	 * @since    1.6
+	 */
+	public $type = 'Station';
+
+	/**
+	 * Method to get the field input markup.
+	 *
+	 * @return    string    The field input markup.
+	 * @since    1.6
+	 */
+	protected function getInput(): string
+	{
+		$wettermodul = ModuleHelper::getModule('mod_dwd_wettermodule');
+		return parent::getInput();
+	}
+}
